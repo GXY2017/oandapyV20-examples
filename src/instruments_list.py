e3c4442ad4bd67a73b962719afdd29286331d156
GXY2017 <29757319+GXY2017@users.noreply.github.com>
2019-04-10 13:19:50 +0800

    Update instruments_list.py

--- src/instruments_list.py
+++ src/instruments_list.py
@@ -5,6 +5,7 @@ import json
 import oandapyV20
 import oandapyV20.endpoints.accounts as accounts
 from exampleauth import exampleAuth
+from pandas.io.json import json_normalize
 
 accountID, token = exampleAuth()
 client = oandapyV20.API(access_token=token)
@@ -12,3 +13,5 @@ client = oandapyV20.API(access_token=token)
 r = accounts.AccountInstruments(accountID=accountID)
 rv = client.request(r)
 print(json.dumps(rv, indent=2))
+all_instru = json_normalize(rv['instruments'])
+
